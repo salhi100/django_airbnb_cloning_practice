997690fadab2e3a0c0633f8f8dd992b9c8b34d1a
noopy <young_ahn@yonsei.ac.kr>
2020-02-14 21:56:00 +0900

    admin.py panel customization completed

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
@@ -6,7 +7,7 @@ from . import models
 
 
 # @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ Custum User Admin"""
 
@@ -14,6 +15,26 @@ class CustomUserAdmin(admin.ModelAdmin):
     # list_display is in description of https://docs.djangoproject.com/en/2.2/ref/contrib/admin/
     # list_display = ("username", "gender", "language", "currency", "superhost")
     # list_filter = ("superhost", "currency", "language")
+
+    # This is to make custom filter(or fieldsets) in Django admin panel.
+    # UserAdmin.fieldsets: default fieldsets provided in Django. CMD + Click to check more information
+    # Tuple added to UserAdmin.fieldsets are my customization
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
     pass
 
 
