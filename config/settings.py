64e4be074d557aef8f753746202d3e815b6d3f1b
noopy <young_ahn@yonsei.ac.kr>
2020-02-27 17:14:32 +0900

    django views.py rendering context, setting up directory paths for html templates in settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],  # where to look for templates
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- rooms/views.py
+++ rooms/views.py
@@ -7,7 +7,12 @@ def all_rooms(request):
     # check specifics of request
     # print(dir(request))
 
-    # now = datetime.now()
+    now = datetime.now()
     # return HttpResponse(content=f"hello {now}")
 
-    return render(request, "all_rooms")
+    hungry = True
+
+    # render is telling Django "go and compile html code into browser html"
+    # context is way of sending variables to html file, as {{variable}}. Logic statements go into {% if %}
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+
