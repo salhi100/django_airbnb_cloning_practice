d81d0c0d5900b4af0e35dcbb001b963141c152a9
noopy <young_ahn@yonsei.ac.kr>
2020-03-12 20:21:02 +0900

    user password authentication, logout(urls.py routing, views.py logout function, nav template) added

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+# exporting user's url routing to django project
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+# get functions from user app's views.py, export them to templates as name="name"
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from . import forms
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/default/#authenticating-users
+from django.contrib.auth import authenticate, login, logout
 
 
 class LoginView(View):
@@ -17,6 +20,17 @@ class LoginView(View):
         # print(form.is_valid())
         if form.is_valid():
             # cleaned data is the cleaned result of all fields
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
 
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/default/#how-to-log-a-user-out
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
