d1b2ad79721f520fac5c07e338033bc6f92cd5e2
noopy <young_ahn@yonsei.ac.kr>
2020-03-18 21:11:25 +0900

    footnotes for tailwind updated & making middle block in home & styling rooms list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+// don't forget to npm run css after making changes
 module.exports = {
   theme: {
-    extend: {},
+    // https://tailwindcss.com/docs/height#customizing
+    // extending to add customized tailwind class
+    extend: {
+      // vh is view height: window size relative to the screen size
+      // tailwind class for vh as {"name of the class": "input for css"}
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      // https://tailwindcss.com/docs/border-radius#customizing
+      borderRadius: {
+        xl: "1.5rem"
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
